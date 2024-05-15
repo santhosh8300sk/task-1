086eac3643ad1c8f6add8968eb33ebac798525dc
santhosh8300sk <168333997+santhosh8300sk@users.noreply.github.com>
2024-05-15 15:11:41 +0530

    Add files via upload

---
+++ Task 1/GradeTracker.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class GradeTracker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of students: ");
+        int numStudents = scanner.nextInt();
+
+        
+        double[] grades = new double[numStudents];
+
+        
+        for (int i = 0; i < numStudents; i++) {
+            System.out.print("Enter grade for student " + (i + 1) + ": ");
+            grades[i] = scanner.nextDouble();
+        }
+
+        
+        double sum = 0;
+        double highest = Double.MIN_VALUE;
+        double lowest = Double.MAX_VALUE;
+
+        for (double grade : grades) {
+            sum += grade;
+            highest = Math.max(highest, grade);
+            lowest = Math.min(lowest, grade);
+        }
+
+        double average = sum / numStudents;
+
+        
+        System.out.println("\nGrade Summary:");
+        System.out.println("Average grade: " + average);
+        System.out.println("Highest grade: " + highest);
+        System.out.println("Lowest grade: " + lowest);
+
+        scanner.close();
+    }
+}
